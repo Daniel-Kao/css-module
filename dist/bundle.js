384c32c04367c89b3bc07b3e3b541814b124a023
daniel <daniel@192.168.124.14>
2019-10-26 23:06:30 +0800

    override all antd styles

--- dist/bundle.js
+++ dist/bundle.js
@@ -1270,7 +1270,7 @@ eval("exports = module.exports = __webpack_require__(/*! ../css-loader/dist/runt
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"body {\\n  background: yellow;\\n}\\n\", \"\"]);\n\n\n//# sourceURL=webpack:///./src/global.css?./node_modules/css-loader/dist/cjs.js");
+eval("exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"body {\\n  background: yellow;\\n}\\n.am-button {\\n  background: green;\\n}\\n\", \"\"]);\n\n\n//# sourceURL=webpack:///./src/global.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
